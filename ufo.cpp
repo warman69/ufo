02e605d90210e242629fdb327bea27334065a94c
Martin Donati <77202547+warman69@users.noreply.github.com>
2021-05-10 13:02:16 +0200

    Create ufo.cpp

---
+++ ufo.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void theyReWatchingUs(int x) {
+
+}
+
+void theyReComing() {
+	int x;
+	while (x --\
+				\
+				 \
+				  \
+				   > 0) {
+		if (x % 9 == 0)
+			printf("🛸	🛸	🛸	🛸	🛸	🛸	🛸	🛸	🛸	🛸");
+		else if (x % 8 == 0)
+			printf("   _____ _    _  _____ _____ ");
+		else if (x % 7 == 0)
+			printf("   _____ _    _  _____ _____ ");
+		else if (x % 6 == 0)
+			printf("  / ____| |  | |/ ____|_   _|");
+		else if (x % 5 == 0)
+			printf(" | |    | |  | | (___   | |");
+		else if (x % 4 == 0)
+			printf(" | |    | |  | |\\___ \\  | |");
+		else if (x % 3 == 0)
+			printf(" | |____| |__| |____) |_| |_");
+		else if (x % 2 == 0)
+			printf("  \\_____|\\____/|_____/|_____|");
+		else if (x % 1 == 0)
+			printf("%s", "KKK0000KKKXXXXXXXXXXXXKKXXXXXXXXXXXXXX0Oxolox00O00KKXXKKKXXXKKKKKKXXXXXXXXXXXKKXXXXNXXXNNXXXXXXXKKK0\nKKKKK0KKKKXXXXXXXXXXXXXXXNNXXXXXXXXX0xol:;;;;::;::cloxO0KXXXXXXXXXXXXXXXXXXXXXXXXXXNNNXXXXXXXXXXKKK0\nKXXXKKKKKKXXXXXXXXXXXXXXXNXXXXXXXXKkl;;:;;;,^,,,;;,,;:ccldkOOKXXKXXXXXXXXXXXXXXXXXXXXXXXXKKKKKKKKKKK\nXKKXXXKKKKXXXXXXXXXXXXXXXXXXXXXXKxl;;;;;:clllloollcc:::::::::ldOKKXXXKXXXXXXXXXXXXXXXKXXXXXXXKKKKKKK\nXKKXXXKKKXXXXXXXXXXXXXXXXXXXKXKkc,^,;cloxxxddoooollllccc:::;,;:lkKKKKKXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\nXXXXXXXKKXXKXXXXXXXXXXXXXXXKX0o,^,:clxkxxkxxkkxxdoooo:^.,::,.,;;okOOOOkkkkOOO00KKKXXXKKKKKKKXKKKKKKK\nXXKKKKKKKKKKKXXXXXXXKKKKXXXKd::cccloxkkOOOOOOOkxdooo:^^;clccclloooooollllcccclloddxkO00KKKXKKKKKKKK\nXXXKKKKKKKXXXKXXXXXXXKKKKXK0xc:::ccldkOOOOOOOOkxdddddoodddddddddddxxdddoolccccc:::;::clodkO0KKKKKKKK\nXXXKKKKXXXXXKKXXXXXXKKKKK0xl:;;;:ccldkO00Okkkkxxxkkkkxxkkkkkkxxxxxxkkxdolcc:;;,,,;^^.....^,:lx0KKKKK\nXXXXKKKXXXXXKKKXXXXKKKKKKkc,;;;:,.^ldxkOkkxkOOO00OOOOkkkkkOOkxdoolc:;,,^.........^^..........^dKXXKK\nXXXXXXXXXXXXKKKKXXXKKKKKOl,,,,,:;^,oxkOO00000KK0OOkkkkkkxdoc;,^.............^^^^^,^......^^^^ckKXXKK\nXXXXXXXXXXXXXXXKKKXXKKKK0d;^,;:loodkO0KKKK00OOOkkxdolc:;;^.................^^^,,;;,...^,;;;:oOKXKKKK\nXXXXXXXXXXXXXXXXKKKXKKKKKkc;cloxkO00000OO0Okxdl:;,^^,;:c:^.... .......^^^,;::cll:;,,;;:::cdkKKKXKKKK\nXNNNNXXXXXXXXXXXKKKKKXK0kdoodxkO000OOOkxol:,^^^,:looddo:............^;:cldxxxdl:,,;:ccloxOKKKXXXKKKK\nXXXXXXXXXXXXXXXXXXXKK0kdooxxkOkkkkkxl:,^......,;cc::,^......^..^^^,,,;:clllc;;;;:clloxO0KKXXKXXXXXKK\nXXXXXXXXXXXXXXXXXXKOxolloxxkxxddl:,...................^^^^^^^,,;;;^..,,^^^,;:cllodkO0KXKKKXXXXXXXXXX\nXXXXXXXXXXXXXXXKKOdlccloddddl:,...............^^^^^..........^;:;^..^^,;:cllooxk0KXXXXXXXXXXXXXXXXXX\nXXXXXXXXXXXXXXKOdcccccodoc;^..............^,^........^^^^^,,,^^^^^,,;:cloodxkOKKXXXXXXXXXXXXXXXXXXXX\nXXXXXXXXXXXXK0xl::ccll:,^....^........^,,;^....^,,;,,;;;,,;:;;;::ccllloxkO0KXXXXXXXXXXXXXXXXXXXXXXXX\nXXXXXXXXXXK0koc::cc;,....^^^^^^.....^,;;;^..^,,;;;;;;;;;;::cclllllldxk0KKKXXXXXXXXXXXXXXXXXXXXXXXXXX\nXXXNNXXXXKOdc:::;,....^^,,;;,^,^...^,;;::,^,;;;:::::ccccclllllodxk0KXXXXXXXXXXXNNXXXXXXXXXXXXXXXXXXX\nNNNNNXXXKkl:;;^.....^,;:ccl:^.^,,^^^..^,,,;;;::ccllooooooodxkO0KXXXXXXXXXXXXNNNNNNXXXXXXXXXXXXXXXXXX\nNNNNNNXKkl:,.......,:clolc;^...^^^^^^,,;::clloddddddxxkkOO0KXXXXXXXXXXXXXXXNNNNNNNNNNNNNNXXXXXXXXXXX\nNNNNNXKkl,..........^,;,^.....^,,;::cccloddxxxxkkOO00KXXXXXXXXXXXXXXNNNNNXNNXXXXXXXXXXXXXXXXXXXXXXNN\nNXXXXX0l^............^,,,;;;::ccclloooodxOO00KKXXXXXXXXXXNXXXXXXXXXNNNNNXXXXXXXNXXXXXXXXXXXXXXXXXXXN\nNNXXXKx,.....^^^^^..^,,;;::ccloodxkkO0KKXXXXXXXXXXXXXXXXNNNNNNNXXXXNNNNNXXXXXXXNXXXXXXXXXXXXXXXXXXXX\nNXXXXX0dlc:;;::;;;:::ccloddkOO00KKXXXXXXXXXXXXXXXNNNNNNNNXXXXXXXXXNNNNNNXXXNNXXNNXXXXXXXXXXXXXXXXXXX\nXXXXXXXXXKK00000000000KKKKKXXXXXXXXXXXXXXXXNXXXNNXXXXXNNNXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXKKK\nXXXXXXXXXXXXXXXXXXXXXXXXX00KNXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXKXXXXXXXXKKKKKKKKKKKKKKKKKK\nXXXXXXXXXXXXXXXXXXXXKKKKOddkKXXXXXXXXXXXXXXXXXXKKXXXXXXXXXXXXXXXXXXXXXXXKKXXKKKXXKKKKKKKKKKKKKK00000\nKXXXXXXXXXXXXXXXXXXK0OkdolodkO00KKKKXXXXXXXXXKKOOKKKKKKKKKKKKKKKKKKKK0000000000000OOOOO0000000000000\n");
+		printf("\n");
+	}
+}
+
+int main() {
+	int val = 10;
+	theyReWatchingUs(val);
+	theyReComing();
+}
